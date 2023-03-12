bd4e1484e2e928afd8d526d8e450c93e467cac1e
Kyle Manning <kyle@manning.jp>
2023-03-12 12:15:49 +0900

    added cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [
-        mdx(), 
-        sitemap(),
-    ],
-    server: {host: true},
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  server: {
+    host: true
+  },
+  output: "server",
+  adapter: cloudflare()
 });
