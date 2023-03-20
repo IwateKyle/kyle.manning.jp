b3a3b87d53e61093a4af240f914e033826f152fa
Kyle Manning <kyle@manning.jp>
2023-03-20 17:57:30 +0900

    added basic block section to front page

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,6 +17,7 @@ const blog = defineCollection({
 			.optional()
 			.transform((str) => (str ? new Date(str) : undefined)),
 		heroImage: z.string().optional(),
+		draft: z.boolean().default(true),
 	}),
 });
 
