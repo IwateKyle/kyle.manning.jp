d1ed11feae0630b3017bb8c589ba49f0066389f6
Kyle Manning <kyle@manning.jp>
2023-04-02 11:42:42 +0900

    updated astro and added svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kyle.manning.jp',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), svelte()]
   // server: {
-    // host: true
+  // host: true
   // },
   // output: "static",
   // adapter: cloudflare()
