6b6f0df1991581310fae84f3c2788444c4dce21f
Kyle Manning <kyle@manning.jp>
2023-04-10 09:01:05 +0900

    added login, register, and logout components

--- src/components/Authentication.svelte
+++ src/components/Authentication.svelte
@@ -1,39 +1,16 @@
 <script>
   import PocketBase from "pocketbase";
-  import { onMount } from "svelte";
-  let pb = new PocketBase("http://localhost:8090");
+  let pb = new PocketBase("http://api.manning.jp");
   let loggedIn = pb.authStore.isValid;
-  let email = "";
-  let password = "";
-  let showLogin = false;
-  let showRegister = false;
 
   const login = async () => {
-    try {
-      await pb.collection("users").authWithPassword(email, password);
-      password = email = "";
-    } catch (err) {}
-    loggedIn = pb.authStore.isValid;
-    showLogin = false;
+    // console.log("trying to login");
+    window.location = "/auth/login";
   };
 
   const register = async () => {
-    try {
-      // example create data
-      const data = {
-        // username: "test_username",
-        email,
-        // emailVisibility: true,
-        password,
-        passwordConfirm: password,
-      };
-      showRegister = false;
-
-      const record = await pb.collection("users").create(data);
-
-      password = email = "";
-    } catch (err) {}
-    loggedIn = pb.authStore.isValid;
+    // console.log("trying to register");
+    window.location = "/auth/register";
   };
 
   const logout = async () => {
@@ -41,149 +18,19 @@
     await pb.authStore.clear();
     loggedIn = pb.authStore.isValid;
   };
-
-  const toggleShowLogin = () => {
-    showLogin = !showLogin;
-  };
-
-  const toggleShowRegister = () => {
-    showRegister = !showRegister;
-  };
 </script>
 
 {#if loggedIn}
-  <span on:click={logout}>Logout</span>
-{:else if showLogin || showRegister}
-  <div class="modal">
-    <div class="backdrop" />
-
-    <div class="content-wrapper">
-      <div>
-        <!-- Modal header content  -->
-      </div>
-
-      <div class="content">
-        <form class="login-form" action="">
-          <!-- <form action="" on:submit|preventDefault={login}> -->
-          <label for="email">username / email</label>
-          <input bind:value={email} type="email" name="email" id="email" />
-          <label for="password">password</label>
-          <input
-            bind:value={password}
-            type="password"
-            name="password"
-            id="password"
-          />
-
-          {#if showLogin}
-            <button class="button" type="button" on:click={login}>login</button>
-            <button class="button" type="button" on:click={toggleShowLogin}
-              >Cancel</button
-            >
-          {/if}
-
-          {#if showRegister}
-            <button class="button" type="submit" on:click={register}
-              >Register</button
-            >
-            <button class="button" type="button" on:click={toggleShowRegister}
-              >Cancel</button
-            >
-          {/if}
-        </form>
-        <!-- content goes here -->
-      </div>
-
-      <div>
-        <!-- Modal footer content  -->
-      </div>
-    </div>
-  </div>
+  <span class="pointer" on:click={logout}>Logout</span>
 {:else}
-  <p>
-    <span on:click={toggleShowLogin}>Login</span> |
-    <span on:click={toggleShowRegister}>Register</span>
+  <p class="pointer">
+    <span on:click={login}>Login</span> |
+    <span on:click={register}>Register</span>
   </p>
 {/if}
 
-<style>
-  div.modal {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100vh;
-    z-index: 20;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  div.backdrop {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(17, 17, 17, 0.45);
-;
-  }
-  div.content-wrapper {
-    z-index: 30;
-    max-width: 70vw;
-    border-radius: 0.3rem;
-    background-color: rgba(17, 17, 17, 0.95);
-
-    overflow: hidden;
-  }
-  div.content {
-    max-height: 50vh;
-    overflow: auto;
-    padding: 1rem;
-  }
-
-  .login-form {
-    display: flex;
-    flex-direction: column;
-  }
-  .button {
-    background-color: #e1ecf4;
-    border-radius: 3px;
-    border: 1px solid #7aa7c7;
-    box-shadow: rgba(255, 255, 255, 0.7) 0 1px 0 0 inset;
-    box-sizing: border-box;
-    color: #39739d;
+<style scoped="true">
+  .pointer {
     cursor: pointer;
-    display: inline-block;
-    font-family: -apple-system, system-ui, "Segoe UI", "Liberation Sans",
-      sans-serif;
-    font-size: 13px;
-    font-weight: 400;
-    line-height: 1.15385;
-    margin: 0;
-    outline: none;
-    padding: 8px 0.8em;
-    position: relative;
-    text-align: center;
-    text-decoration: none;
-    user-select: none;
-    -webkit-user-select: none;
-    touch-action: manipulation;
-    vertical-align: baseline;
-    white-space: nowrap;
-  }
-
-  .button:hover,
-  .button:focus {
-    background-color: #b3d3ea;
-    color: #2c5777;
-  }
-
-  .button:focus {
-    box-shadow: 0 0 0 4px rgba(0, 149, 255, 0.15);
-  }
-
-  .button:active {
-    background-color: #a0c7e4;
-    box-shadow: none;
-    color: #2c5777;
   }
 </style>
---
+++ src/components/RegisterForm.svelte
@@ -0,0 +1,279 @@
+<script lang="js">
+  import PocketBase from "pocketbase";
+  import * as assert from "assert/strict";
+
+  let username = "";
+  let name = "";
+  let email = "";
+  let password = "";
+  let passwordConfirm = "";
+  let errorMessage = "";
+  let errorClass = "errorNone";
+
+  const register = async () => {
+    errorClass = "errorNone";
+    errorMessage = "";
+    if (password !== passwordConfirm) {
+      errorMessage =
+        "Oops, the two passwords you entered didn't match.  Please re-enter them and click submit again. :)";
+      errorClass = "error";
+      return;
+    }
+    const pb = new PocketBase("https://api.manning.jp");
+
+    const data = {
+      username,
+      email,
+      emailVisibility: false,
+      password,
+      passwordConfirm,
+      name,
+    };
+
+    const record = await pb
+      .collection("users")
+      .create(data)
+      .catch((e) => {
+        errorClass = "error";
+        errorMessage = e;
+      });
+  };
+</script>
+
+<div class="login-box">
+  <h2>Register</h2>
+  <div class={errorClass}>
+    {errorMessage}
+  </div>
+
+  <form>
+    <div class="user-box">
+      <input bind:value={name} type="text" name="name" required="true" />
+      <label for="name">Real Name</label>
+    </div>
+    <div class="user-box">
+      <input
+        bind:value={username}
+        type="text"
+        name="username"
+        required="true"
+      />
+      <label for="username">Username</label>
+    </div>
+    <div class="user-box">
+      <input bind:value={email} type="email" name="email" required="true" />
+      <label for="email">Email</label>
+    </div>
+    <div class="user-box">
+      <input
+        bind:value={password}
+        type="password"
+        name="password"
+        required="true"
+      />
+      <label for="password">Password</label>
+    </div>
+
+    <div class="user-box">
+      <input
+        bind:value={passwordConfirm}
+        type="password"
+        name="password-verified"
+        required="true"
+      />
+      <label for="password-verified">Password</label>
+    </div>
+    <!-- svelte-ignore a11y-invalid-attribute -->
+    <a on:click={register} href="#">
+      <span />
+      <span />
+      <span />
+      <span />
+      Submit
+    </a>
+  </form>
+</div>
+
+<style>
+  html {
+    height: 100%;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    background: linear-gradient(#141e30, #243b55);
+  }
+
+  .error {
+    background-color: red;
+    color: #eee;
+    padding: 0.25rem;
+    border-radius: 4px;
+    transition: 0.2s;
+    transform: scaleY(1);
+  }
+  .errorNone {
+    transform: scaleY(0);
+  }
+
+  .login-box {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 400px;
+    padding: 40px;
+    transform: translate(-50%, -50%);
+    background: rgba(0, 0, 0, 0.5);
+    box-sizing: border-box;
+    box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
+    border-radius: 10px;
+  }
+
+  .login-box h2 {
+    margin: 0 0 30px;
+    padding: 0;
+    color: #fff;
+    text-align: center;
+  }
+
+  .login-box .user-box {
+    position: relative;
+  }
+
+  .login-box .user-box input {
+    width: 100%;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    margin-bottom: 30px;
+    border: none;
+    border-bottom: 1px solid #fff;
+    outline: none;
+    background: transparent;
+  }
+  .login-box .user-box label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    pointer-events: none;
+    transition: 0.5s;
+  }
+
+  .login-box .user-box input:focus ~ label,
+  .login-box .user-box input:valid ~ label {
+    top: -20px;
+    left: 0;
+    color: #03e9f4;
+    font-size: 12px;
+  }
+
+  .login-box form a {
+    position: relative;
+    display: inline-block;
+    padding: 10px 20px;
+    color: #03e9f4;
+    font-size: 16px;
+    text-decoration: none;
+    text-transform: uppercase;
+    overflow: hidden;
+    transition: 0.5s;
+    margin-top: 40px;
+    letter-spacing: 4px;
+  }
+
+  .login-box a:hover {
+    background: #03e9f4;
+    color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 5px #03e9f4, 0 0 25px #03e9f4, 0 0 50px #03e9f4,
+      0 0 100px #03e9f4;
+  }
+
+  .login-box a span {
+    position: absolute;
+    display: block;
+  }
+
+  .login-box a span:nth-child(1) {
+    top: 0;
+    left: -100%;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(90deg, transparent, #03e9f4);
+    animation: btn-anim1 1s linear infinite;
+  }
+
+  @keyframes btn-anim1 {
+    0% {
+      left: -100%;
+    }
+    50%,
+    100% {
+      left: 100%;
+    }
+  }
+
+  .login-box a span:nth-child(2) {
+    top: -100%;
+    right: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(180deg, transparent, #03e9f4);
+    animation: btn-anim2 1s linear infinite;
+    animation-delay: 0.25s;
+  }
+
+  @keyframes btn-anim2 {
+    0% {
+      top: -100%;
+    }
+    50%,
+    100% {
+      top: 100%;
+    }
+  }
+
+  .login-box a span:nth-child(3) {
+    bottom: 0;
+    right: -100%;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(270deg, transparent, #03e9f4);
+    animation: btn-anim3 1s linear infinite;
+    animation-delay: 0.5s;
+  }
+
+  @keyframes btn-anim3 {
+    0% {
+      right: -100%;
+    }
+    50%,
+    100% {
+      right: 100%;
+    }
+  }
+
+  .login-box a span:nth-child(4) {
+    bottom: -100%;
+    left: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(360deg, transparent, #03e9f4);
+    animation: btn-anim4 1s linear infinite;
+    animation-delay: 0.75s;
+  }
+
+  @keyframes btn-anim4 {
+    0% {
+      bottom: -100%;
+    }
+    50%,
+    100% {
+      bottom: 100%;
+    }
+  }
+</style>
